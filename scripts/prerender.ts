012efe6104ca358dd9bdd4be7c2106b86cde2452
Josin Simon <josinsimon15@gmail.com>
2026-08-14 12:03:41 +0530

    feat: add Google Analytics GA4 tracking script

--- scripts/prerender.ts
+++ scripts/prerender.ts
@@ -41,6 +41,14 @@ function buildDocument(
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Google Analytics (GA4) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2CT0YQ1VHW"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-2CT0YQ1VHW');
+    </script>
     <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
     <link rel="manifest" href="/manifest.json" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
